d0c2871947e114092c794b1aed53557a8ed9fe37
Benjamin DUMONT <benjamindumont@live.com>
2025-03-23 20:31:58 +0100

    update version

--- jito_protos/src/convert.rs
+++ jito_protos/src/convert.rs
@@ -5,7 +5,8 @@ use std::{
 };
 
 use bincode::serialize;
-use solana_perf::packet::{Packet, PacketBatch, PACKET_DATA_SIZE};
+use solana_perf::packet::Packet;
+use solana_perf::packet::{PacketBatch, PACKET_DATA_SIZE};
 use solana_sdk::{
     packet::{Meta, PacketFlags},
     transaction::VersionedTransaction,
@@ -33,7 +34,8 @@ pub fn packet_to_proto_packet(p: &Packet) -> Option<ProtoPacket> {
                 forwarded: p.meta().forwarded(),
                 repair: p.meta().repair(),
                 simple_vote_tx: p.meta().is_simple_vote_tx(),
-                tracer_packet: p.meta().is_tracer_packet(),
+                tracer_packet: p.meta().is_perf_track_packet(),
+                from_staked_node: p.meta().is_from_staked_node(),
             }),
             sender_stake: 0,
         }),
@@ -69,7 +71,10 @@ pub fn proto_packet_to_packet(p: &ProtoPacket) -> Packet {
                 packet.meta_mut().flags.insert(PacketFlags::FORWARDED);
             }
             if flags.tracer_packet {
-                packet.meta_mut().flags.insert(PacketFlags::TRACER_PACKET);
+                packet
+                    .meta_mut()
+                    .flags
+                    .insert(PacketFlags::PERF_TRACK_PACKET);
             }
             if flags.repair {
                 packet.meta_mut().flags.insert(PacketFlags::REPAIR);
@@ -136,22 +141,5 @@ impl TryFrom<&Socket> for SocketAddr {
 
     fn try_from(value: &Socket) -> Result<Self, Self::Error> {
         IpAddr::from_str(&value.ip).map(|ip| SocketAddr::new(ip, value.port as u16))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use solana_perf::test_tx::test_tx;
-    use solana_sdk::transaction::VersionedTransaction;
-
-    use crate::convert::{proto_packet_from_versioned_tx, versioned_tx_from_packet};
-
-    #[test]
-    fn test_proto_to_packet() {
-        let tx_before = VersionedTransaction::from(test_tx());
-        let tx_after = versioned_tx_from_packet(&proto_packet_from_versioned_tx(&tx_before))
-            .expect("tx_after");
-
-        assert_eq!(tx_before, tx_after);
     }
 }
